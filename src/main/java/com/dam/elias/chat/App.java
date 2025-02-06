de7ef9f295932af58962cfcb6c20ec3e603d6cbc
Elias Martín González <70772713+eliazzz12@users.noreply.github.com>
2025-02-06 19:04:03 +0100

    Abrir app con la ventana de login

--- src/main/java/com/dam/elias/chat/App.java
+++ src/main/java/com/dam/elias/chat/App.java
@@ -1,31 +1,33 @@
 package com.dam.elias.chat;
 
-import com.dam.elias.chat.api.model.ChatManagerGUI;
-import com.dam.elias.chat.gui.controllers.AppController;
+import com.dam.elias.chat.client.api.connection.LoginManagerGUI;
+import com.dam.elias.chat.client.api.model.ChatManagerGUI;
+import com.dam.elias.chat.client.gui.controllers.AppController;
+import com.dam.elias.chat.client.gui.controllers.LoginController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 500, 340);
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 350, 450);
+        stage.setScene(scene);
+        LoginController controller;
         try {
-            AppController controller = fxmlLoader.getController();
-            ChatManagerGUI cm = ChatManagerGUI.getInstance(getParameters(), controller);
-            controller.setChatManager(cm);
+            controller = fxmlLoader.getController();
+            LoginManagerGUI lm = LoginManagerGUI.getInstance(getParameters(), controller);
+            controller.setLoginManager(lm);
+            controller.setup();
         } catch (IOException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
         }
         stage.setTitle("Chat TCP Elias");
-        stage.setScene(scene);
         stage.show();
     }
 
