0fa3b35342ed0ec40a7e3241fcff99a13f905db8
Elias Martín González <70772713+eliazzz12@users.noreply.github.com>
2025-02-07 09:52:22 +0100

    implementar getOtherUser

--- src/main/java/com/dam/elias/chat/client/api/model/PrivateChat.java
+++ src/main/java/com/dam/elias/chat/client/api/model/PrivateChat.java
@@ -1,16 +1,57 @@
 package com.dam.elias.chat.client.api.model;
 
+import com.dam.elias.chat.client.api.model.exceptions.UserNotInThisChatException;
+
+import java.util.Objects;
+
 public class PrivateChat extends Chat {
-    private User clientUser, otherUser;
+    private User firstUser, secondUser;
 
-    public PrivateChat(User clientUser, User otherUser) {
+    public PrivateChat(User localUser, User otherUser) {
         super(otherUser.getUsername());
+        setUser(firstUser);
+        setUser(secondUser);
     }
 
-    public User getOtherUser() {
-        return otherUser;
+    /**
+     * Returns the other user in this chat
+     * @param sender one user of the chat
+     * @return the other user
+     * @throws UserNotInThisChatException
+     */
+    public User getOtherUser(User sender) throws UserNotInThisChatException {
+        if(firstUser.equals(sender)) {
+            return secondUser;
+        } else if(secondUser.equals(sender)) {
+            return firstUser;
+        }
+        throw new UserNotInThisChatException();
     }
 
+    public void setUser(User user) {
+        if(user == null){
+            throw new IllegalArgumentException("A user cannot be set to null");
+        }
+        if(firstUser == null){
+            firstUser = user;
+        } else if(secondUser == null) {
+            secondUser = user;
+        } else {
+            throw new IllegalArgumentException("All users are already set");
+        }
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        PrivateChat that = (PrivateChat) o;
+        return Objects.equals(firstUser, that.firstUser) && Objects.equals(secondUser, that.secondUser);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), firstUser, secondUser);
+    }
 }
